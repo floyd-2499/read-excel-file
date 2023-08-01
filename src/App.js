a85f1f5f916ff967f28ccbbc503596efd9e01650
Floyd Manuel Dsouza <floyd.dsouza@esgds.io>
2023-08-01 18:53:10 +0530

    read excel and console in JSON format

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import * as xlsx from "xlsx"
 
 function App() {
+  const [excelFile, setExcelFile] = useState()
+
+  const refFileTypes = ['application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.ms-excel', 'text/csv']
+
+  const fetchData = (e) => {
+    const file = e.target.files[0]
+
+    if(file && refFileTypes.includes(file.type)) {
+      const readFile = new FileReader();
+      readFile.readAsArrayBuffer(file)
+      readFile.onload=(e)=>{
+        setExcelFile(e.target.result)
+      }
+    }
+  }
+
+  const fileSubmit = (e) => {
+    e.preventDefault();
+    const workBook = xlsx.read(excelFile, {type: 'buffer'})
+    const sheetName = workBook.SheetNames[0]
+    const sheet = workBook.Sheets[sheetName]
+    const outputData = xlsx.utils.sheet_to_json(sheet)
+    console.log(outputData);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={fileSubmit}>
+        <input type='file'className='upload' required onChange={fetchData} />
+        <button type="submit" className='submit-button'>Submit</button>
+      </form>
     </div>
   );
 }
